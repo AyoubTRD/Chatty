b018b48c12bc7f34262bcb2307f66446945fef36
JrTRD <ayoubtrdone@gmail.com>
2019-08-13 16:03:25 +0200

    first-commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+const $ = element => document.querySelector(element);
+const $$ = element => document.querySelectorAll(element);
+$$("form").forEach(form => {
+  form.addEventListener("submit", e => e.preventDefault());
+});
+
+let name = localStorage.getItem("name");
+
+if (name) $(".name").remove();
+else {
+  $(".name").addEventListener("submit", () => {
+    if ($(".name").name.value) {
+      localStorage.setItem("name", $(".name").name.value);
+      name = localStorage.getItem("name");
+      $(".name").remove();
+    }
+  });
+}
+
+const showMessages = (message, id) => {
+  let ownerClass = "";
+  let owner = message.author;
+  if (message.author == name) {
+    ownerClass = "own-message";
+    owner = "Me";
+  }
+
+  let html = `
+  <li class="px-4 my-2 ${ownerClass}" data-id="${id}">
+    <span class="author pt-2 d-block">${owner}</span>
+    <p class="text pb-2">${message.text}</p>
+  </li>
+  `;
+  $("ul").innerHTML += html;
+};
+
+const sendMessage = text => {
+  const message = {
+    author: name,
+    text: text
+  };
+  db.collection("messages").add(message);
+};
+
+db.collection("messages").onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    if (change.type == "added") {
+      showMessages(change.doc.data(), change.doc.id);
+    }
+  });
+});
+
+$(".message").addEventListener("submit", () => {
+  if ($(".message").text.value && name) {
+    sendMessage($(".message").text.value);
+    $(".message").text.value = "";
+  }
+});
